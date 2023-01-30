bd3be90f8410b62a70f96d56cb03ed01a68b0e9d
mae916 <ruchia916@naver.com>
2023-01-30 18:06:04 +0900

    2.4 jsx 문법

---
+++ hello-react/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+// import {fragment} from 'react';
+
+function App() {
+  return (
+    <>
+      <h1>리액트 하이</h1>
+      <h2>작동 한다</h2>
+    </>
+  );
+}
+
+export default App;
